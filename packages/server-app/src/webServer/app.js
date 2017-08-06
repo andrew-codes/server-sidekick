1ceb2cc04263b767e8d600b059514a6b18ae8ca7
Andrew Smith <andrew@andrew.codes>
2017-08-06 19:58:14 -0400

    Exclude serving JS files and HMR requests for web server

--- packages/server-app/src/webServer/app.js
+++ packages/server-app/src/webServer/app.js
@@ -1,8 +1,15 @@
+import unless from 'express-unless';
 import loggerWare from './middlewares/loggerWare';
 import reactPagesWare from './middlewares/reactPagesWare';
 
 export default (server) => {
     server.use(loggerWare);
-    server.use(reactPagesWare);
+    reactPagesWare.unless = unless;
+    server.use(reactPagesWare.unless({
+        path: [
+            '/dist/client/index.js',
+            '/__webpack_hmr'
+        ],
+    }));
     return server;
 }
