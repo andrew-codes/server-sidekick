14c30dbdf59dea2ee0bfb6a718f274060bc8cf96
Andrew Smith <andrew@andrew.codes>
2017-08-06 15:36:43 -0400

    Include API app as git submodule

---
+++ build/bin/restore-api.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+BASEDIR=$(dirname $0)
+PKG="ServerStatus"
+
+$BASEDIR/_execute-scope-command.sh "$PKG" "../../build/bin/restore-api.execute.sh $@"
---
+++ build/bin/start-api.execute.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+pushd ../.. > /dev/null
+export PATH="`npm bin`:$PATH"
+popd > /dev/null
+
+chalk --no-stdin -t "{blue Running API App}"
+dotnet run
