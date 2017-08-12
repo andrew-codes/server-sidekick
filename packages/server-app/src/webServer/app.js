aa99e4769ef68e12ef7c29f86a5429c0585ed863
Andrew Smith <andrew@andrew.codes>
2017-08-12 14:35:36 -0400

    Include logo in web app

--- packages/server-app/src/webServer/app.js
+++ packages/server-app/src/webServer/app.js
@@ -17,6 +17,7 @@ export default (server) => {
         const extensionsToIgnore = [
             'jpg',
             'css',
+            'png',
             'js',
             'ico',
             'json',
--- packages/web/src/components/ApplicationBar.js
+++ packages/web/src/components/ApplicationBar.js
@@ -1,19 +1,17 @@
 import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui-icons/Menu';
 import React from 'react';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import {createStyleSheet, withStyles} from 'material-ui/styles';
 
 const styleSheet = createStyleSheet({
-    root: {
-        marginTop: 30,
-        width: '100%',
+    title: {
+        paddingLeft: '10px',
     },
 });
 
 const ApplicationBar = ({
+                            classes,
                             failed,
                             title,
                         }) => (
@@ -22,7 +20,9 @@ const ApplicationBar = ({
         position="fixed"
     >
         <Toolbar>
+            <img src="/static/hackweek-serversidekick-icon.png" />
             <Typography
+                className={classes.title}
                 color="inherit"
                 type="title"
             >
